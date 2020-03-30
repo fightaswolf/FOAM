48cd29471ee23406dfcd0d713c65845c743bb2e2
fightaswolf <62863508+fightaswolf@users.noreply.github.com>
2020-03-30 11:31:22 +0700

    Update foam_installer.sh

--- foam_installer.sh
+++ foam_installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
-sudo add-apt-repository http://dl.openfoam.org/ubuntu
-sudo apt-get update
-sudo apt-get install openfoam6
-sudo apt-get install build-essential
+sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+add-apt-repository http://dl.openfoam.org/ubuntu
+apt-get update
+apt-get install openfoam6
+apt-get install build-essential
 echo ". /opt/openfoam6/etc/bashrc" >> $HOME/.bashrc
 . $HOME/.bashrc
 simpleFoam -help
 echo "export DISPLAY=:0" >> ${HOME}/.bashrc
 . $HOME/.bashrc
-sudo apt-get install gedit gedit-plugins
-sudo apt-get install gnuplot gnuplot-x11 gnuplot-doc libgd-tools
+apt-get install gedit gedit-plugins
+apt-get install gnuplot gnuplot-x11 gnuplot-doc libgd-tools
